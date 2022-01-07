1b0765e09beeaa75d16ee55765662655de307adf
sidalieleuldj <sidali.eleuldj@gmail.com>
2022-01-07 17:26:27 +0100

    es6

---
+++ script/app.js
@@ -0,0 +1,24 @@
+// script 1
+
+const students = [
+  { name: "ADEL", score: 10 },
+  { name: "SOFIANE", score: 9 },
+  { name: "KARIM", score: 12 },
+];
+
+const result = students.map (students => `${students.name} has score ${students.score}`)
+
+console.log(result);
+
+
+
+// // script 2
+
+// let numberList = [1,2, 3];
+
+// let newList = numberList.map(function (st) {
+//     return st * 2;
+    
+// })
+
+// // console.log(newList, numberList);
